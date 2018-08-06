11c3386fd5e5cfaa74d74614513d4200bef00b90
Ansar Gondal <ansarababs395@gmail.com>
2018-08-07 03:47:26 +0500

    Updates the @index view to show all the data in table

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-md-12">
                 <a href="#upload-csv-modal" data-toggle="modal" class="btn btn-primary mb-3">Upload CSV</a>
-                <table class="table table-bordered table-hover">
+                <table class="table table-bordered table-hover table-responsive">
                     <thead>
                     <tr>
                         <th>ID</th>
@@ -16,8 +16,41 @@
                         <th>Type</th>
                         <th>EAN</th>
                         <th>SKU</th>
-                        <th>Brand</th>
-                        <th>Title</th>
+                        <th>CLEAN SKU</th>
+                        <th>SKU TYPE</th>
+                        <th>CATEGORY</th>
+                        <th>BRAND</th>
+                        <th>TITLE</th>
+                        <th>CONDITION</th>
+                        <th>OUR INVENTORY QUANTITY</th>
+                        <th>MSP</th>
+                        <th>MXSP</th>
+                        <th>OUR PRICE</th>
+                        <th>OUR SALE PRICE</th>
+                        <th>NEW PRICE</th>
+                        <th>PRICE DIFFERENCE</th>
+                        <th>SELLER PRICE</th>
+                        <th>SELLER SALE PRICE</th>
+                        <th>SELLER</th>
+                        <th>SELLER RATING</th>
+                        <th>OTHER OFFER</th>
+                        <th>AVAILABLE OFFER</th>
+                        <th>TOP SELLER</th>
+                        <th>SELLER HANDLING TIME</th>
+                        <th>AVAILABLE OFFERS</th>
+                        <th>TOP EAN</th>
+                        <th>OUR HANDLING TIME</th>
+                        <th>INVENTORY STATUS</th>
+                        <th>PRICE UPDATED AT</th>
+                        <th>PRICE CHECKED AT</th>
+                        <th>COMMENT</th>
+                        <th>LAST CHECKED BY</th>
+                        <th>FB STATUS</th>
+                        <th>IS ACTIVE</th>
+                        <th>ACTION REQUESTED AT</th>
+                        <th>ACTION PENDING</th>
+                        <th>CREATED AT</th>
+                        <th>UPDATED AT</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -28,8 +61,41 @@
                             <td>{{$list->type}}</td>
                             <td>{{$list->ean}}</td>
                             <td>{{$list->sku}}</td>
+                            <td>{{$list->clean_sku}}</td>
+                            <td>{{$list->sku_type}}</td>
+                            <td>{{$list->category}}</td>
                             <td>{{$list->brand}}</td>
                             <td>{{$list->title}}</td>
+                            <td>{{$list->condition}}</td>
+                            <td>{{$list->our_inventory_quantity}}</td>
+                            <td>{{$list->msp}}</td>
+                            <td>{{$list->mxsp}}</td>
+                            <td>{{$list->our_price}}</td>
+                            <td>{{$list->our_sale_price}}</td>
+                            <td>{{$list->new_price}}</td>
+                            <td>{{$list->price_difference}}</td>
+                            <td>{{$list->seller_price}}</td>
+                            <td>{{$list->seller_sale_price}}</td>
+                            <td>{{$list->seller}}</td>
+                            <td>{{$list->seller_rating}}</td>
+                            <td>{{$list->other_offer}}</td>
+                            <td>{{$list->top_seller}}</td>
+                            <td>{{$list->seller_handling_time}}</td>
+                            <td>{{$list->available_offers}}</td>
+                            <td>{{$list->available_offers}}</td>
+                            <td>{{$list->top_ean}}</td>
+                            <td>{{$list->our_handling_time}}</td>
+                            <td>{{$list->inventory_status}}</td>
+                            <td>{{$list->price_updated_at}}</td>
+                            <td>{{$list->price_checked_at}}</td>
+                            <td>{{$list->comment}}</td>
+                            <td>{{$list->last_checked_by}}</td>
+                            <td>{{$list->fb_status}}</td>
+                            <td>{{$list->is_active}}</td>
+                            <td>{{$list->action_requested_at}}</td>
+                            <td>{{$list->action_pending}}</td>
+                            <td>{{$list->created_at}}</td>
+                            <td>{{$list->updated_at}}</td>
                         </tr>
                     @endforeach
                     </tbody>
